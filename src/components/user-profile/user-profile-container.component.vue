3242ea8c205152a4fa17f291f0c050da43a14892
Hallison Melo <hallison.pm@gmail.com>
2024-03-02 14:39:15 -0300

    feat: menu options

--- src/components/user-profile/user-profile-container.component.vue
+++ src/components/user-profile/user-profile-container.component.vue
@@ -7,10 +7,21 @@
       src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
       alt=""
     />
-    <div class="mt-4">
-      <h6 class="text-lg font-bold text-gray-600">Hallison Melo</h6>
-      <h6 class="text-md font-normal text-gray-500">masculino, solteiro</h6>
-      <h6 class="text-md font-normal text-gray-500">Brasil</h6>
-    </div>
+    <div class="mt-4"></div>
+    <ul class="text-sm font-medium text-gray-500 bg-white">
+      <li class="w-full px-4 py-5 border-b border-gray-200 rounded-t-lg">
+        <h6 class="text-lg font-bold text-gray-600">Hallison Melo</h6>
+        <h6 class="text-md font-normal text-gray-500">masculino, solteiro</h6>
+        <h6 class="text-md font-normal text-gray-500">Brasil</h6>
+      </li>
+      <li class="w-full px-4 py-2 rounded-b-lg">Adicionar</li>
+      <li class="w-full px-4 py-2 border-b border-gray-200">Denunciar</li>
+      <li class="w-full px-4 py-2 rounded-b-lg">Perfil</li>
+      <li class="w-full px-4 py-2 rounded-b-lg">Recados (3)</li>
+      <li class="w-full px-4 py-2 rounded-b-lg">Galeria (40)</li>
+      <li class="w-full px-4 py-2 rounded-b-lg">Depoimentos (39)</li>
+      <li class="w-full px-4 py-2 rounded-b-lg">Recados</li>
+      <li class="w-full px-4 py-2 rounded-b-lg">Aplicativos (2)</li>
+    </ul>
   </div>
 </template>
