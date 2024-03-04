610863f28c11db709cf08876ed74b4ebadcc6f0c
Hallison Melo <hallison.pm@gmail.com>
2024-03-04 15:50:17 -0300

    fix: responsive design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid justify-items-center mb-8">
     <navbar-component />
-    <div class="max-w-xs sm:max-w-prose lg:max-w-screen-xl mt-10">
+    <div class="max-w-sm sm:max-w-prose lg:max-w-screen-xl mt-10">
       <div class="mt-20">
         <div class="grid grid-cols-12 gap-x-4 gap-y-4">
           <first-column-component />
