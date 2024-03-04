23e9aa3463092b3fe3f9d9ecac02394035b2feff
Hallison Melo <hallison.pm@gmail.com>
2024-03-04 15:57:22 -0300

    fix: responsive user informations

--- src/components/user-info/user-informations/user-information-item.component.vue
+++ src/components/user-info/user-informations/user-information-item.component.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="max-w-screen-xl mt-4">
     <div class="grid grid-cols-12 gap-x-4 gap-y-4">
-      <div class="col-span-3 text-left">
+      <div class="col-span-4 sm:col-span-3 text-left">
         <span class="text-gray-500">relacionamento</span>
       </div>
-      <div class="col-span-9 text-left">
+      <div class="col-span-8 sm:col-span-9 text-left">
         <span class="text-gray-700"> solteiro </span>
       </div>
 
-      <div class="col-span-3 text-left">
+      <div class="col-span-4 sm:col-span-3 text-left">
         <span class="text-gray-500">quem sou eu</span>
       </div>
-      <div class="col-span-9 text-left">
+      <div class="col-span-8 sm:col-span-9 text-left">
         <span class="text-gray-700">
           Lorem ipsum lobortis aliquam a gravida lectus malesuada, purus curae
           vitae magna nullam et eget nullam, nec potenti congue aliquet fusce
@@ -21,17 +21,17 @@
         </span>
       </div>
 
-      <div class="col-span-3 text-left">
+      <div class="col-span-4 sm:col-span-3 text-left">
         <span class="text-gray-500">país</span>
       </div>
-      <div class="col-span-9 text-left">
+      <div class="col-span-8 sm:col-span-9 text-left">
         <span class="text-gray-700"> Brasil </span>
       </div>
 
-      <div class="col-span-3 text-left">
+      <div class="col-span-4 sm:col-span-3 text-left">
         <span class="text-gray-500">música</span>
       </div>
-      <div class="col-span-9 text-left flex flex-wrap">
+      <div class="col-span-8 sm:col-span-9 text-left flex flex-wrap">
         <div
           class="py-1 px-4 bg-white border border-pink-400 rounded-large text-gray-700 me-3 mb-3"
           v-for="(item, index) in musics"
@@ -41,10 +41,10 @@
         </div>
       </div>
 
-      <div class="col-span-3 text-left">
+      <div class="col-span-4 sm:col-span-3 text-left">
         <span class="text-gray-500">filmes</span>
       </div>
-      <div class="col-span-9 text-left flex flex-wrap">
+      <div class="col-span-8 sm:col-span-9 text-left flex flex-wrap">
         <span
           class="py-1 px-4 bg-white border border-pink-400 rounded-large text-gray-700 me-3 mb-3"
           v-for="(item, index) in films"
@@ -54,10 +54,10 @@
         </span>
       </div>
 
-      <div class="col-span-3 text-left">
+      <div class="col-span-4 sm:col-span-3 text-left">
         <span class="text-gray-500">livros</span>
       </div>
-      <div class="col-span-9 text-left flex flex-wrap">
+      <div class="col-span-8 sm:col-span-9 text-left flex flex-wrap">
         <span
           class="py-1 px-4 bg-white border border-pink-400 rounded-large text-gray-700 me-3 mb-3"
           v-for="(item, index) in books"
