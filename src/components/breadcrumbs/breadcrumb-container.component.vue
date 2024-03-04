6b506a5c3d3901dd969e6c783b38240c0c427938
Hallison Melo <hallison.pm@gmail.com>
2024-03-04 12:10:46 -0300

    feat: breadcrumb start

---
+++ src/components/breadcrumbs/breadcrumb-container.component.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="flex p-0" aria-label="Breadcrumb">
+    <ol
+      class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse"
+    >
+      <li class="inline-flex items-center">
+        <a
+          href="#"
+          class="inline-flex items-center text-sm font-medium text-gray-700"
+        >
+          Início
+        </a>
+      </li>
+      <li>
+        <div class="flex items-center">
+          <svg
+            class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 9 4-4-4-4"
+            />
+          </svg>
+          <a href="#" class="ms-1 text-sm font-bold text-pink-500">Perfil</a>
+        </div>
+      </li>
+    </ol>
+  </nav>
+</template>
--- src/components/user-info/user-info-container.component.vue
+++ src/components/user-info/user-info-container.component.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="p-6 bg-white border border-gray-200 rounded-large shadow">
-    <p>Hallison Melo</p>
+    <breadcrumbs />
   </div>
 </template>
+<script lang="ts" setup>
+import Breadcrumbs from "@/components/breadcrumbs/breadcrumb-container.component.vue";
+</script>
