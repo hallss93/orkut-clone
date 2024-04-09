f8f09129a38a4d522cbe3a1c6cd980adb1160247
Hallison Melo <hallss93@hotmail.com>
2024-04-09 19:58:22 -0300

    fix: responsive layout

--- src/views/templates/Home/HomeView.vue
+++ src/views/templates/Home/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid justify-items-center mb-8">
     <navbar-component />
-    <div class="max-w-sm sm:max-w-prose lg:max-w-screen-xl mt-10">
+    <div class="w-11/12 lg:w-11/12 mt-10 xl:w-10/12 2xl:w-8/12">
       <div class="mt-20">
         <div class="grid grid-cols-12 gap-x-4 gap-y-4">
           <router-view />
