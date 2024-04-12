e618721c81ee548bdabe2c58cdfdbd027f322c56
Hallison Melo <hallss93@hotmail.com>
2024-04-12 16:01:08 -0300

    feat: responsive bio

--- src/views/components/atoms/user-informations/user-information-item.component.vue
+++ src/views/components/atoms/user-informations/user-information-item.component.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="max-w-screen-xl mt-4">
-    <div class="grid grid-cols-12 gap-x-4 gap-y-4">
-      <div class="col-span-4 sm:col-span-3 text-left">
+    <div class="grid grid-cols-12 gap-x-4 sm:gap-y-4 gap-y-0">
+      <div
+        class="col-span-12 sm:col-span-3 sm:py-0 py-2 sm:px-0 px-4 sm:text-left text-start sm:bg-white bg-blue-50 rounded-lg"
+      >
         <span class="text-gray-500">relacionamento</span>
       </div>
-      <div class="col-span-8 sm:col-span-9 text-left">
+      <div class="col-span-8 sm:col-span-9 text-left sm:py-0 py-2 sm:px-0 px-4">
         <span class="text-gray-700"> solteiro </span>
       </div>
 
-      <div class="col-span-4 sm:col-span-3 text-left">
+      <div
+        class="col-span-12 sm:col-span-3 sm:py-0 py-2 sm:px-0 px-4 sm:text-left text-start sm:bg-white bg-blue-50 rounded-lg"
+      >
         <span class="text-gray-500">quem sou eu</span>
       </div>
-      <div class="col-span-8 sm:col-span-9 text-left">
+      <div class="col-span-8 sm:col-span-9 text-left sm:py-0 py-2 sm:px-0 px-4">
         <span class="text-gray-700">
           Lorem ipsum lobortis aliquam a gravida lectus malesuada, purus curae
           vitae magna nullam et eget nullam, nec potenti congue aliquet fusce
@@ -21,17 +25,23 @@
         </span>
       </div>
 
-      <div class="col-span-4 sm:col-span-3 text-left">
+      <div
+        class="col-span-12 sm:col-span-3 sm:py-0 py-2 sm:px-0 px-4 sm:text-left text-start sm:bg-white bg-blue-50 rounded-lg"
+      >
         <span class="text-gray-500">país</span>
       </div>
-      <div class="col-span-8 sm:col-span-9 text-left">
+      <div class="col-span-8 sm:col-span-9 text-left sm:py-0 py-2 sm:px-0 px-4">
         <span class="text-gray-700"> Brasil </span>
       </div>
 
-      <div class="col-span-4 sm:col-span-3 text-left">
+      <div
+        class="col-span-12 sm:col-span-3 sm:py-0 py-2 sm:px-0 px-4 sm:text-left text-start sm:bg-white bg-blue-50 rounded-lg"
+      >
         <span class="text-gray-500">música</span>
       </div>
-      <div class="col-span-8 sm:col-span-9 text-left flex flex-wrap">
+      <div
+        class="col-span-8 sm:col-span-9 text-left sm:py-0 py-2 sm:px-0 px-4 flex flex-wrap"
+      >
         <tag-item-component
           v-for="(item, index) in musics"
           :key="index"
@@ -41,10 +51,14 @@
         </tag-item-component>
       </div>
 
-      <div class="col-span-4 sm:col-span-3 text-left">
+      <div
+        class="col-span-12 sm:col-span-3 sm:py-0 py-2 sm:px-0 px-4 sm:text-left text-start sm:bg-white bg-blue-50 rounded-lg"
+      >
         <span class="text-gray-500">filmes</span>
       </div>
-      <div class="col-span-8 sm:col-span-9 text-left flex flex-wrap">
+      <div
+        class="col-span-8 sm:col-span-9 text-left sm:py-0 py-2 sm:px-0 px-4 flex flex-wrap"
+      >
         <tag-item-component
           v-for="(item, index) in films"
           :key="index"
@@ -54,10 +68,14 @@
         </tag-item-component>
       </div>
 
-      <div class="col-span-4 sm:col-span-3 text-left">
+      <div
+        class="col-span-12 sm:col-span-3 sm:py-0 py-2 sm:px-0 px-4 sm:text-left text-start sm:bg-white bg-blue-50 rounded-lg"
+      >
         <span class="text-gray-500">livros</span>
       </div>
-      <div class="col-span-8 sm:col-span-9 text-left flex flex-wrap">
+      <div
+        class="col-span-8 sm:col-span-9 text-left sm:py-0 py-2 sm:px-0 px-4 flex flex-wrap"
+      >
         <tag-item-component
           v-for="(item, index) in books"
           :key="index"
--- src/views/components/organisms/user-info/user-info-bio.component.vue
+++ src/views/components/organisms/user-info/user-info-bio.component.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
-    <p class="text-xl font-bold text-start mt-4">Hallison Melo</p>
+  <div class="text-center">
+    <avatar-image-component
+      rounded
+      hight="h-40"
+      width="w-40"
+      class="block lg:hidden"
+    />
+    <p class="text-xl font-bold text-center lg:text-start mt-4">
+      Hallison Melo
+    </p>
     <div class="py-2 px-4 text-start bg-blue-50 rounded-lg mt-4">
       Minha mensagem de bio
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import AvatarImageComponent from "@/views/components/atoms/avatar-image.component.vue";
+</script>
