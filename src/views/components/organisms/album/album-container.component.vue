fe06739bd8f24bf85b748a3a79663242ec8cc142
Hallison Melo <hallss93@hotmail.com>
2024-04-12 17:04:13 -0300

    feat: custom style active slide item

--- src/views/components/organisms/album/album-container.component.vue
+++ src/views/components/organisms/album/album-container.component.vue
@@ -105,3 +105,45 @@ function slideTo(val: number) {
 if (!route.params.id) router.push("/gallery/");
 currentSlide.value = Number(route.params.id);
 </script>
+
+<style scoped>
+.carousel__slide {
+  padding: 5px;
+}
+
+.carousel__viewport {
+  perspective: 2000px;
+}
+
+.carousel__track {
+  transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+  transition: 0.5s;
+}
+
+.carousel__slide {
+  opacity: 0.9;
+  transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active ~ .carousel__slide {
+  transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+  opacity: 1;
+  transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+  opacity: 1;
+  transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+  opacity: 1;
+  transform: rotateY(0) scale(1.1);
+}
+</style>
